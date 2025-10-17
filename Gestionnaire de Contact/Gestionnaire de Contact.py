332f70583c53955eaeefad8b1f36baa8236045e8
Louis Bertagna <louisbrtgn@gmail.com>
2025-10-18 01:26:33 +0200

    Add comments to clarify contact manager functions
    Added descriptive comments throughout the code to explain the purpose of each function and key variables. This improves code readability and maintainability for future developers.

--- Gestionnaire de Contact/Gestionnaire de Contact.py
+++ Gestionnaire de Contact/Gestionnaire de Contact.py
@@ -1,11 +1,12 @@
-from json import load, dump
-from time import sleep
+from json import load, dump  # Importation des fonctions pour manipuler les fichiers JSON
+from time import sleep  # Importation de la fonction sleep pour faire des pauses
 
-# Fichier de sauvegarde
-SAVE_FILE = "Gestionnaire de Contact\Liste de contact - Sauvegarde.json"
+SAVE_FILE = "Gestionnaire de Contact\Liste de contact - Sauvegarde.json"  # Chemin du fichier de sauvegarde des contacts
 
-def fonct_choix():
+def fonct_choix():  # Fonction pour afficher le menu et obtenir le choix de l'utilisateur
+
     choix = 0
+
     while choix not in (1, 2, 3, 4):
         choix = int(input(""" 
 Que voulez-vous faire ?
@@ -19,14 +20,17 @@ Que voulez-vous faire ?
     4. Quitter
 
 Votre choix : """ ))
+
         if choix in (1, 2, 3, 4):
             return choix
+
         else:
             print("\nChoix Invalide")
             sleep(2)
 
 
-def ajouter_modifier_contact():
+def ajouter_modifier_contact():  # Fonction pour ajouter ou modifier un contact
+
     nom = input("\nEntrez le nom du contact à ajouter ou à modifier : ")
     numero = input("Entrez le numéro de téléphone du contact : ")
     
@@ -41,12 +45,12 @@ def ajouter_modifier_contact():
 
 
 
-def supprimer_contact(nom):
+def supprimer_contact(nom):  # Fonction pour supprimer un contact
 
     with open(SAVE_FILE, "r", encoding='utf-8') as f:
         data = load(f)
     
-    if nom == "*":
+    if nom == "*":  # Supprimer tous les contacts
         data.clear()
         with open(SAVE_FILE, "w", encoding='utf-8') as f:
             dump(data, f)
@@ -60,19 +64,23 @@ def supprimer_contact(nom):
             dump(data, f)
 
         return f"\nLe contact {nom} a été supprimé."
+
     else:
         return f"\nLe contact {nom} n'existe pas."
 
 
 
-def afficher_contacts(nom):
+def afficher_contacts(nom):  # Fonction pour afficher un contact
+
     with open(SAVE_FILE, "r", encoding='utf-8') as f:
         data = load(f)
-    if nom == "*":
+
+    if nom == "*":  # Afficher tous les contacts
         print("\nAffichage de tout les contacts :")
         for i in data:
             print(f"\n  {i} : {data[i]}")
         return ""
+
     if nom in data:
         return f"\nLe numéro de {nom} est {data[nom]}."
     else:
